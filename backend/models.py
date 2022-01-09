d63d503be5b4fc5e284579c2ee86c7d6f0a2c576
Paras <soodparassood4@gmail.com>
2022-01-09 23:28:29 +0530

    Made models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from rest_framework.serializers import ModelSerializer
 
-# Create your models here.
+class Item(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.DecimalField(decimal_places=2,max_digits=7)
+    quantity=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class ItemSerializer(ModelSerializer):
+    class Meta:
+        model=Item
+        fields="__all__"
