0fb33d91e28491506f884734ff1477d443df2e01
Paras <soodparassood4@gmail.com>
2022-01-09 23:30:04 +0530

    added urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from backend import views
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('itemApi',views.ItemAPIView.as_view(),name="itemApi"),
+    path('itemApi/<item_id>',views.ItemAPIView.as_view())
+]
