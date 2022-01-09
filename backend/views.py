81d469a9b1394916159cea03feac0a590dfeefb9
Paras <soodparassood4@gmail.com>
2022-01-09 23:28:58 +0530

    Made api

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,44 @@
+from django.http.response import Http404
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import json
+from rest_framework import status
+from backend.models import Item, ItemSerializer
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+class ItemAPIView(APIView):
+    def get(self,request):
+        items=Item.objects.all()
+        serializer=ItemSerializer(items,many = True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        item_serializer=ItemSerializer(data=json.loads(request.body))
+        if item_serializer.is_valid():
+            item_serializer.save()
+            return Response("Item saved successfully")
+        return Response(item_serializer.errors)
+
+    def put(self,request,item_id):
+        try:
+            item=Item.objects.get(pk=item_id)
+        except Item.DoesNotExist:
+            raise Http404
+        serializer=ItemSerializer(item,data=json.loads(request.body))
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Updated")
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,item_id):
+        try:
+            item=Item.objects.get(pk=item_id)
+            name=str(item)
+        except Item.DoesNotExist:
+            raise Http404
+        item.delete()
+        return Response(f"Deleted item = {item}")
